33e190615ae0402e780d8b763666c060b9195952
Daniel <dcdanny03@gmail.com>
2024-05-13 17:41:29 -0400

    [debugging] debugging git

--- recipefinder/src/main/java/personal/project/recipefinder/RecipefinderApplication.java
+++ recipefinder/src/main/java/personal/project/recipefinder/RecipefinderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipefinderApplication {
 
-    // Spring Application boot to run application
+    // Spring Application boot
     public static void main ( final String[] args ) {
         SpringApplication.run( RecipefinderApplication.class, args );
     }
