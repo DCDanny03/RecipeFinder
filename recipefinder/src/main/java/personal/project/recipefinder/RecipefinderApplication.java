b7be1fe2103658a25d8d97cbfc46ec3d402fb9dd
dcdanny03 <dcdanny03@gmail.com>
2024-05-13 17:46:11 -0400

    [Broken]

--- recipefinder/src/main/java/personal/project/recipefinder/RecipefinderApplication.java
+++ recipefinder/src/main/java/personal/project/recipefinder/RecipefinderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipefinderApplication {
 
-    // Spring Application boot bruh
+    // Spring Application boot
     public static void main ( final String[] args ) {
         SpringApplication.run( RecipefinderApplication.class, args );
     }
