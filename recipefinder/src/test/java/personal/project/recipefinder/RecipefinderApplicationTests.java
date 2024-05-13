74ab6c04d17d4d175a26b8cb4ac41775fdc19d79
Daniel Cao <dcdanny03@gmail.com>
2024-05-13 17:35:28 -0400

    [SetUp] Spring Initializer added working project

---
+++ recipefinder/src/test/java/personal/project/recipefinder/RecipefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.project.recipefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
